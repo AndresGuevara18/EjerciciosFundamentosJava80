4775be11595e5edddc326857e0d0e1436699bae1
andres-1891 <mauricio_acastro@soy.sena.edu.co>
2023-11-30 16:36:11 -0500

    se quita56

--- src/Ejercicio55.java
+++ src/Ejercicio55.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Ejercicio55 {
     public static void main(String[] args) {
-        double number1 = 0,  cant = 0, positive = 0, negative=0, neu=0;
+        double number1 = 0;
+        int cant = 0, positive = 0, negative=0, neu=0;
         Scanner src = new Scanner(System.in);
 
         /*55. Construya el algoritmo que permita el ingreso de una serie de números y que determine
@@ -13,7 +14,7 @@ public class Ejercicio55 {
                 "cuantos números positivos, cuantos números negativos y cuantos ceros hay. Como primer\n" +
                 "dato pedir la cantidad de números que forman la lista.");
         System.out.println("Ingrese la cantidad de números");
-        cant = src.nextDouble();
+        cant = src.nextInt();
 
         for(int i=1; i <= cant; i++){
             System.out.println("Ingrese un número #"+i);
