ef037769fd493be74be59bb460d3d91f14c9b5ee
andres-1891 <mauricio_acastro@soy.sena.edu.co>
2023-11-30 15:59:42 -0500

    Se agrega 55

--- src/Ejercicio55.java
+++ src/Ejercicio55.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ejercicio55 {
     public static void main(String[] args) {
-        double number1 = 0, number2 = 0, cant = 0;
+        double number1 = 0,  cant = 0, positive = 0, negative=0, neu=0;
         Scanner src = new Scanner(System.in);
 
         /*55. Construya el algoritmo que permita el ingreso de una serie de números y que determine
@@ -16,10 +16,21 @@ public class Ejercicio55 {
         cant = src.nextDouble();
 
         for(int i=1; i <= cant; i++){
-            System.out.println("Ingrese un número");
+            System.out.println("Ingrese un número #"+i);
             number1 = src.nextDouble();
+
+            if(number1 > 0){
+                positive = positive+1;
+            }else if(number1 < 0){
+                negative = negative+1;
+            }else{
+                neu = neu+1;
+            }
         }
 
-        
+        System.out.println("Ingreso "+positive+" número positivos.");
+        System.out.println("Ingreso "+negative+" número negativos.");
+        System.out.println("Ingreso "+neu+" número neutros.");
+
     }
 }
